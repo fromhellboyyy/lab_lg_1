3cdda5160bc78ae235761b9c0401fae5b3761bdf
Ilya Yampolskiy <hellboy@niuitmo.ru>
2024-12-11 00:59:53 +0300

    fix pomoika ot gpt <3

--- scratch_1.py
+++ scratch_1.py
@@ -1,11 +1,14 @@
-class csr:
-    def create_matrix():
+class Csr:
+    def __init__(self, val, col_ind, row_ptr, scalar):
+        self.val = val
+        self.col_ind = col_ind
+        self.row_ptr = row_ptr
+        self.scalar = scalar
+
+    def create_matrix(self):
         m = int(input())
         n = int(input())
-        val = []
         trace = 0
-        col_ind = []
-        row_ind = [0]
         matrix = list()
         for iterations in range(n):
             matrix += [list(map(int, input().split()))]
@@ -13,29 +16,46 @@ class csr:
             counter = 0
             for j in range(m):
                 if matrix[i][j] != 0:
-                    val.append(matrix[i][j])
-                    col_ind.append(j)
+                    self.val.append(matrix[i][j])
+                    self.col_ind.append(j)
                     counter += 1
-            row_ind.append(row_ind[i] + counter)
-        return [val, col_ind, row_ind]
+            self.row_ptr.append(self.row_ptr[i] + counter)
+        return [self.val, self.col_ind, self.row_ptr]
 
-
-print(csr.create_matrix())
-#meow
-class csr:
-    def __init__(self, val, col_ind, row_ptr):  # Исправлено на row_ptr
-        self.val = val
-        self.col_ind = col_ind
-        self.row_ptr = row_ptr  # Исправлено на row_ptr
+    def mol(self, other):
+        assert len(self.col_ind) == len(
+            other.row_ptr) - 1, "Невозможное умножение: количество столбцов первой матрицы должно быть равно количеству строк второй."
+        n = len(self.row_ptr) - 1  # Количество строк в первой матрице
+        m = len(other.col_ind) - 1  # Количество столбцов во второй матрице
+        result_val = []
+        result_col_ind = []
+        result_row_ptr = [0]
+        for i in range(n):
+            row_result = {}
+            for j in range(self.row_ptr[i], self.row_ptr[i + 1]):
+                a_val = self.val[j]
+                a_col = self.col_ind[j]
+                for k in range(other.row_ptr[a_col], other.row_ptr[a_col + 1]):
+                    b_val = other.val[k]
+                    b_row = other.col_ind[k]
+                    if b_row not in row_result:
+                        row_result[b_row] = 0
+                    row_result[b_row] += a_val * b_val
+            for self.col_ind, self.val in row_result.items():
+                result_val.append(val)
+                result_col_ind.append(col_ind)
+            result_row_ptr.append(len(result_val))
+        self.val = result_val
+        self.col_ind = result_col_ind
+        self.row_ptr = result_row_ptr
+        return Csr.create_matrix(self.n, self.m, result_val, self.col_ind, self.row_ptr)
 
     def add(self, other):
         if len(self.row_ptr) != len(other.row_ptr):
             raise ValueError("Матрицы должны иметь одинаковое число строк.")
-
         result_val = []
         result_col_ind = []
-        result_row_ptr = [0]  # Исправлено на row_ptr
-
+        result_row_ptr = [0]
         i = 0
         j = 0
         while i < len(self.val) or j < len(other.val):
@@ -52,55 +72,18 @@ class csr:
                 result_col_ind.append(self.col_ind[i])
                 i += 1
                 j += 1
-
             if i == self.row_ptr[len(result_row_ptr) - 1]:
                 result_row_ptr.append(len(result_val))
+        return Csr.create_matrix(result_val, result_col_ind, result_row_ptr)
 
-        return csr(result_val, result_col_ind, result_row_ptr)
-
-    def mol_by_scalar(matrix):
-        val = matrix[0]
-        col_ind = matrix[1]
-        row_ind = matrix[2]
-        if scalar!=0:
-            new_val = [element * scalar for element in val]
-            return new_val, col_ind, row_ind
-        return [[], [], []]
-
-
-
-def mol(self, other):
-    assert len(self.col_ind) == len(other.row_ind) - 1, "Невозможное умножение: количество столбцов первой матрицы должно быть равно количеству строк второй."
-
-    n = len(self.row_ind) - 1  # Количество строк в первой матрице
-    m = len(other.col_ind) - 1  # Количество столбцов во второй матрице
-
-    result_vals = []
-    result_col_ind = []
-    result_row_ind = [0]
-
-    for i in range(n):
-        row_result = {}
-
-        for j in range(self.row_ind[i], self.row_ind[i + 1]):
-            A_val = self.val[j]
-            A_col = self.col_ind[j]
-
-            for k in range(other.row_ind[A_col], other.row_ind[A_col + 1]):
-                B_val = other.val[k]
-                B_row = other.col_ind[k]
-
-                if B_row not in row_result:
-                    row_result[B_row] = 0
-                row_result[B_row] += A_val * B_val
-
-        for col, val in row_result.items():
-            result_vals.append(val)
-            result_col_ind.append(col)
-
-        result_row_ind.append(len(result_vals))
-
-    return CSR(n, m, result_vals, result_col_ind, result_row_ind)
+def mol_by_scalar(self, matrix):
+    matrix_val = matrix[0]
+    matrix_col_ind = matrix[1]
+    matrix_row_ptr = matrix[2]
+    if self.scalar != 0:
+        new_val = [element * self.scalar for element in matrix_val]
+        return new_val, matrix_col_ind, matrix_row_ptr
+    return [[], [], []]
 
 
 def determinant(matrix):
@@ -110,17 +93,22 @@ def determinant(matrix):
     if n == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
-    f=0
+    f = 0
     for j in range(n):
         minor = [row[:j] + row[j + 1:] for row in matrix[1:]]  # Удаляем первую строку и j-й столбец
         det += (-1) ** j * matrix[0][j] * determinant(minor)  # Рекурсия для вычисления детерминанта
-        if det==0:
-            f=1
+        if det == 0:
+            f = 1
+    return det, f
 
-
-    return [det, f]
 
 if f==1:
     print(det,'Да')
 else:
     print('Нет')
+val = []
+col_ind = []
+row_ptr = [0]
+scalar = 0
+pdiddy = Csr(val, col_ind, row_ptr, scalar)
+pdiddy.create_matrix()
