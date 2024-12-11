6f60a3dc36f7d839eb50d7508a35e113d772cc21
EuGeron52 <snow.golem.mur@gmail.com>
2024-12-11 16:05:14 +0300

    sex2:  redemption

--- scratch_1.py
+++ scratch_1.py
@@ -2,11 +2,11 @@ class Csr:
     def __init__(self):
         self.val = []
         self.col_ind = []
-        self.row_ptr = []
+        self.row_ptr = [0]
 
     def create_matrix(self):
-        m = int(input())
         n = int(input())
+        m = int(input())
         matrix = list()
         for iterations in range(n):
             matrix += [list(map(int, input().split()))]
@@ -21,32 +21,34 @@ class Csr:
         return [self.val, self.col_ind, self.row_ptr]
 
     def mol(self, other):
-        assert len(self.col_ind) == len(
-            other.row_ptr) - 1, "Невозможное умножение: количество столбцов первой матрицы должно быть равно количеству строк второй."
+        assert max(self.col_ind) != len(other.row_ptr) - 1, "Невозможное умножение: количество столбцов первой матрицы должно быть равно количеству строк второй."
         n = len(self.row_ptr) - 1  # Количество строк в первой матрице
         m = len(other.col_ind)  # Количество столбцов во второй матрице
+        val = self.val
+        col_ind = self.col_ind
+        row_ptr = self.row_ptr
         result_val = []
         result_col_ind = []
         result_row_ptr = [0]
         for i in range(n):
             row_result = {}
-            for j in range(self.row_ptr[i], self.row_ptr[i + 1]):
-                a_val = self.val[j]
-                a_col = self.col_ind[j]
+            for j in range(row_ptr[i], row_ptr[i + 1]):
+                a_val = val[j]
+                a_col = col_ind[j]
                 for k in range(other.row_ptr[a_col], other.row_ptr[a_col + 1]):
                     b_val = other.val[k]
                     b_row = other.col_ind[k]
                     if b_row not in row_result:
                         row_result[b_row] = 0
                     row_result[b_row] += a_val * b_val
-            for self.col_ind, self.val in row_result.items():
-                result_val.append(self.val)
-                result_col_ind.append(self.col_ind)
+            for col_ind, val in row_result.items():
+                result_val.append(val)
+                result_col_ind.append(col_ind)
             result_row_ptr.append(len(result_val))
         return [result_val, result_col_ind, result_row_ptr]
 
     def add(self, other):
-        if len(self.row_ptr) != len(other.row_ptr):
+        if (len(self.row_ptr) != len(other.row_ptr)) or (max(self.col_ind) != max(other.col_ind)):
             raise ValueError("Матрицы должны иметь одинаковое число строк.")
         result_val = []
         result_col_ind = []
@@ -93,5 +95,24 @@ class Csr:
         for j in range(n):
             minor = [row[:j] + row[j + 1:] for row in matrix[1:]]  # Удаляем первую строку и j-й столбец
             det += (-1) ** j * matrix[0][j] * Csr.determinant(self, minor)  # Рекурсия для вычисления детерминанта
-pdiddy = Csr()
-pdiddy.create_matrix()
+
+    def csr_to_normal(self, csr_matrix):
+        col_ind = csr_matrix[1]
+        row_ind = csr_matrix[2]
+        print(max(col_ind))
+        print(len(row_ind))
+        matrix = [[0 for i in range(max(col_ind) + 1)] for j in range(len(row_ind) - 1)]
+        val = csr_matrix[0]
+        for i in range(len(val)):
+            col = col_ind[i]
+            row = 0
+            for j in range(len(row_ind) - 1):
+                if i >= row_ind[j] and i < row_ind[j + 1]:
+                    row = j
+            matrix[row][col] = val[i]
+        return matrix
+p = Csr()
+print(p.create_matrix())
+d = Csr()
+print(d.create_matrix())
+print(p.add(d))
